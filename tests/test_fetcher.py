f4e95396ab133a12410cd92fdedc20efb0916fe6
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2023-01-03 12:33:41 +0200

    refactor fetcher

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -5,12 +5,14 @@ from web_scraper import fetcher
 
 
 @pytest.fixture
-def markup() -> str:
-    raw_html: str = ''
+def sample_path():
+    return str(pathlib.Path('tests/samples/markup.html').resolve())
 
-    with pathlib.Path('tests/samples/markup.html').open('r') as file:
-        raw_html = file.read()
-    return raw_html
+
+@pytest.fixture
+def markup(sample_path) -> bytes:
+    with pathlib.Path(sample_path).open('rb') as file:
+        return file.read()
 
 
 @pytest.fixture
@@ -19,10 +21,6 @@ def url() -> str:
 
 
 class MockRequests:
-    def __init__(self) -> None:
-        self.content = ''
-
-    # @staticmethod
     def get(self, markup):
         self.content = markup
         return self
@@ -31,9 +29,38 @@ class MockRequests:
 def test_can_fetch_html_content_from_url(markup, url, monkeypatch):
     def mock_get(*args, **kwargs):
         return MockRequests().get(markup)
-
     monkeypatch.setattr(requests, 'get', mock_get)
+
     expected = markup
     actual = fetcher.Fetcher().fetch_from_internet(url)
 
     assert expected == actual
+
+
+def test_can_fetch_html_from_local(markup, sample_path):
+
+    expected = markup
+    actual = fetcher.Fetcher().fetch_from_local(sample_path)
+
+    assert actual == expected
+
+
+def test_can_extract_html(tmp_path, sample_path, markup):
+    temp_dir = tmp_path / 'tests'
+    temp_dir.mkdir()
+    temp_dir = temp_dir / 'samples'
+    temp_dir.mkdir()
+
+    f = fetcher.Fetcher()
+    f.fetch_from_local(sample_path)
+    f.extract_html(str(temp_dir))
+
+    file_path = pathlib.Path(temp_dir/'result.html')
+
+    expected = markup
+    actual = ''
+    with file_path.open('rb') as file:
+        actual = file.read()
+
+    assert pathlib.Path(temp_dir/'result.html').exists()
+    assert actual == expected
--- web_scraper/fetcher.py
+++ web_scraper/fetcher.py
@@ -6,19 +6,21 @@ from bs4 import BeautifulSoup
 class Fetcher:
     '''Fetch raw html from internet or local'''
 
-    def fetch_from_internet(self, url) -> bytes:
-        '''Return raw bytes of html from internet'''
+    def __init__(self) -> None:
+        self.markup = ''
 
-        return requests.get(url).content
+    def fetch_from_internet(self, url) -> bytes:
+        '''Return raw bytes of html from internet and populate self.markup with markup bytes'''
+        self.markup = requests.get(url).content
+        return self.markup
 
     def fetch_from_local(self, path: str) -> bytes:
-        '''Return raw bytes of html from local'''
-        pass
+        '''Return raw bytes of html from local and populate self.markup with markup bytes'''
+        with pathlib.Path(path).open('rb') as file:
+            self.markup = file.read()
+        return self.markup
 
     def extract_html(self, output_path: str, output_filename: str = 'result.html') -> None:
-        '''Save html to output_path/output_filename.html (result.html by default)
-
-        The result is sanitize with beautiful soup html parser'''
-        html = BeautifulSoup(self.markup, 'html.parser').prettify()
-        with pathlib.Path(output_path+'/'+output_filename).open('w') as file:
-            file.write(html)
+        '''Save self.markup to output_path/output_filename.html (result.html by default)'''
+        with pathlib.Path(output_path+'/'+output_filename).open('wb') as file:
+            file.write(self.markup)
