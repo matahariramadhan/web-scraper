2a0898b3a99d6e34ff47f827f272f9c127ea95b0
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2023-01-07 13:34:01 +0200

    add new test to test_fetcher to increase coverage

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -45,22 +45,31 @@ def test_can_fetch_html_from_local(markup, sample_path):
     assert actual == expected
 
 
-def test_can_extract_html(tmp_path, sample_path, markup):
-    temp_dir = tmp_path / 'tests'
-    temp_dir.mkdir()
-    temp_dir = temp_dir / 'samples'
-    temp_dir.mkdir()
+class TestExtractHtml:
+    @pytest.fixture
+    def temp_dir(self, tmp_path) -> str:
+        temp_dir = tmp_path / 'tests'
+        temp_dir.mkdir()
+        temp_dir = temp_dir / 'samples'
+        temp_dir.mkdir()
+        return str(temp_dir)
 
-    f = fetcher.Fetcher()
-    f.fetch_from_local(sample_path)
-    f.extract_html(str(temp_dir))
+    def test_can_extract_html(self, temp_dir, sample_path, markup):
+        f = fetcher.Fetcher()
+        f.fetch_from_local(sample_path)
+        f.extract_html(str(temp_dir))
 
-    file_path = pathlib.Path(temp_dir/'result.html')
+        file_path = pathlib.Path(temp_dir+'/'+'result.html')
 
-    expected = markup
-    actual = ''
-    with file_path.open('rb') as file:
-        actual = file.read()
+        expected = markup
+        actual = ''
+        with file_path.open('rb') as file:
+            actual = file.read()
 
-    assert pathlib.Path(temp_dir/'result.html').exists()
-    assert actual == expected
+        assert pathlib.Path(temp_dir+'/'+'result.html').exists()
+        assert actual == expected
+
+    def test_extract_html_raise_exception(self, temp_dir):
+        with pytest.raises(Exception):
+            f = fetcher.Fetcher()
+            f.extract_html(temp_dir)
