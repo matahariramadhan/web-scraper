59f88b62c2e46a8abd751e010163682b2cef34ac
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2023-01-26 17:34:42 +0200

    refactor automate_test_preparation

--- automate_test_preparation.py
+++ automate_test_preparation.py
@@ -1,7 +1,5 @@
 import pathlib
 import requests
-from web_scraper import Scraper, Fetcher, ShamelaParser, XMLSaver
-from bs4 import BeautifulSoup
 
 
 def create_markup_html(url):
@@ -9,27 +7,10 @@ def create_markup_html(url):
         markup.write(requests.get(url).content)
 
 
-def create_result_xml(url, output_path):
-    scraper = Scraper(Fetcher, ShamelaParser, XMLSaver)
-    scraper.fetch_from_internet(url).parse_content().save(output_path)
-
-
-def create_nass_dict(url):
-    raw_html = Fetcher().fetch_from_internet(url)
-    soup = BeautifulSoup(raw_html, 'html.parser')
-    nass = soup.find('div', class_='nass')
-
-    with pathlib.Path('tests/samples/nass.txt').open('w') as file:
-        file.write(str(nass))
-
-
 if __name__ == '__main__':
     url = 'https://shamela.ws/book/23599/612'
     try:
         create_markup_html(url)
-        create_result_xml(
-            url, '/home/matahari/development/projects/web-scraping/tests/samples')
-        create_nass_dict(url)
 
         print('Successfully prepare test!')
     except Exception as e:
